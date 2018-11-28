1143ebff0a24efd0b2ce09e63ed267860f01aa80
EricLee <ericlee@EricLeede-MBP.frontierlocal.net>
2018-11-27 17:49:12 -0800

    Add Layout component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+import Layout from "./components/Layout"
+
+ReactDOM.render(<Layout/>, document.getElementById('root'));
